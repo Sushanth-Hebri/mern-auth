8411b63f4ca79868fc23a33d5f056523fd8d28a1
Sushanth <sushanthhebri336@gmail.com>
2024-01-20 14:54:58 +0530

    create test aoi route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 
 // Connect to MongoDB
@@ -8,13 +9,18 @@ const password = process.env.PASSWORD;
 const encodedPassword = encodeURIComponent(password);
 const mongoURI = `mongodb+srv://sushanthhebri336:${encodedPassword}@mern.ftbhaxu.mongodb.net/mern-auth?retryWrites=true&w=majority`;
 
-mongoose.connect(mongoURI).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((err) => {
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
+
+app.use("/api/user", userRoutes);
