8274c326581ef3dd540cb5b82705ef05a4a0204b
Sushanth <sushanthhebri336@gmail.com>
2024-01-20 13:21:30 +0530

    mongodb connected

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// Connect to MongoDB
+const password = process.env.PASSWORD;
+const encodedPassword = encodeURIComponent(password);
+const mongoURI = `mongodb+srv://sushanthhebri336:${encodedPassword}@mern.ftbhaxu.mongodb.net/mern-auth?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoURI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
 
 const app = express();
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Server listening on port 3000');
-})
+});
